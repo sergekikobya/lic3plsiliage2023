82580fe0243b1874c38ba2f46d1b761ddf752dfe
sergekikobya <sergekikobya@gmail.com>
2023-04-11 15:39:39 +0200

    Creation de la page index.php
    Juste squelette de la page

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Exercices PHP</title>
+</head>
+<body>
+<?php
+//Commentaire mono-ligne
+/* Comentaire
+multi-ligne */
+
+
+?>
+
+</body>
+</html>
